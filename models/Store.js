2e0649da1b7dd21563c5a1fd28c01d393a5ce120
Jason Vance <jbvance@gmail.com>
2017-05-13 10:13:46 -0500

    Add Store model

---
+++ models/Store.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+});
+
+storeSchema.pre('save', function(next) {
+  if (!this.isModified('name')){
+    next(); //skip it, name has not been changed, so no
+            //need to update the slug
+    return; //stop function from continuing
+  }
+  //this = the store instance we are trying to save
+  this.slug = slug(this.name);
+  next();
+  //TODO make sure slugs are unique so no duplicate store name slugs
+});
+
+module.exports = mongoose.model('Store', storeSchema);
